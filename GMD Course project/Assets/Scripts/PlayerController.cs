f4703b4608d9ff87436aab8939fbd8fb3310ad4b
Kasper Steinmeyer Jensen <kasperscoop@gmail.com>
2023-03-11 11:55:04 +0100

    added simple movement to player

--- GMD Course project/Assets/Scripts/PlayerController.cs
+++ GMD Course project/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController _controller;
-    private Vector3 _playerVelocity;
-    private float _speed = 5f;
+    [SerializeField]
+    private float speed = 10f;
     private Vector3 rawInputMovement;
     private Rigidbody rb;
 
     // Start is called before the first frame update
     private void Start()
     {
-        _controller = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -20,12 +19,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        rb.AddForce(rawInputMovement);
+        rb.AddForce(rawInputMovement*speed);
     }
 
-    public void OnMovement(InputValue movementValue)
+    public void OnMove(InputValue movementValue)
     {
         var inputMovement = movementValue.Get<Vector2>();
         rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y);
+    }
+
+    private void OnFire(InputValue fireValue)
+    {
+        Debug.Log("LEFT CLICK");
+    }
+    
+    private void OnAlternateFire(InputValue fireValue)
+    {
+        Debug.Log("RIGHT CLICK");
+    }
+    
+    private void OnLook(InputValue lookValue)
+    {
+        Debug.Log("We looked! "+lookValue.Get());
     }
 }
